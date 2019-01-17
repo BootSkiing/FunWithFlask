b6310eb4690e5bebdff12bc348f904fbba3ec72a
Connor Jackson <connortheguy12@gmail.com>
2019-01-16 23:52:07 -0500

    Now with POST!

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+WORD= "Hello, World!"
 
-@app.route("/")
+@app.route("/users", methods=["GET"])
 def home():
-    return "Hello, World!"
+    with open("word.txt") as f:
+        return f.readline()
 
+@app.route("/users", methods=["POST"])
+def add():
+    with open("word.txt", "w+") as f:
+        f.write("Holy poop, it wrote")
+    return '200'
